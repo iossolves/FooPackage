ccc6c22bff2a74d8dae3c8cba2a8a159c7089610
Nandana <nandana@conscala.com>
2023-05-10 15:15:49 +0530

    Added a Method

--- Sources/FooPackage/FooPackage.swift
+++ Sources/FooPackage/FooPackage.swift
@@ -5,24 +5,12 @@ import Appwrite
 
 struct FooPackage {
 
-    static let client = Client()
-    static let account = Account(client)
-
-    public static func login(
-        endpoint: String,
-        projectId: String,
-        email: String,
-        password: String,
-        completion: @escaping (Result<Session, AppwriteError>) -> Void
-    ) {
-//        client
-//            .setEndpoint(endpoint)
-//            .setProject(projectId)
-//
-//        account.createSession(
-//            email: email,
-//            password: password,
-//            completion: completion
-//        )
+    public static func login() {
+        print("This is from Foo Package-login!")
     }
+    
+    public static func foo() {
+        print("This is from Foo Package!")
+    }
+
 }
