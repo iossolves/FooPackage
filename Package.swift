d56d90b3ea524139af26d0a02d5f62956832a2a1
iOS Solves <132999821+iossolves@users.noreply.github.com>
2023-05-10 13:20:46 +0530

    Add files via upload

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version: 5.7.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FooPackage",
+    platforms: [
+        .iOS("15.0"),
+        .macOS("11.0"),
+        .watchOS("7.0"),
+        .tvOS("13.0"),
+    ],
+    products: [
+        .library(
+            name: "FooPackage",
+            targets: ["FooPackage"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/appwrite/sdk-for-swift", from: "2.0.0")
+    ],
+    targets: [
+        .target(
+            name: "FooPackage",
+            dependencies: [
+                .product(name: "Appwrite", package: "sdk-for-swift")
+            ]),
+        .testTarget(
+            name: "FooPackageTests",
+            dependencies: [
+                "FooPackage"
+            ]
+        )
+    ]
+)
